d3ce9c9a96a7a6816cb635910d2042ae3b2e8661
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2024-07-01 13:28:03 -0500

    feat: se agrega el flatlist y sectionlist

--- src/components/FlatList/index.ts
+++ src/components/FlatList/index.ts
@@ -1,1 +1,11 @@
+import { factory } from '../factory';
+import { FlatList as NativeFlatList } from 'react-native';
+import type { FlatListProps } from './types';
+
 export type { FlatListFactoryConfiguration, FlatListProps } from './types';
+
+function createFlatList<ItemT>() {
+  return factory<FlatListProps<ItemT>>(NativeFlatList, 'flatList');
+}
+
+export const FlatList = createFlatList();
--- src/components/SectionList/index.ts
+++ src/components/SectionList/index.ts
@@ -1,4 +1,17 @@
+import { factory } from '../factory';
+import { SectionList as NativeSectionList } from 'react-native';
+import type { SectionListProps } from './types';
+
 export type {
   SectionListFactoryConfiguration,
   SectionListProps,
 } from './types';
+
+function createSectionList<ItemT, SectionT>() {
+  return factory<SectionListProps<ItemT, SectionT>>(
+    NativeSectionList,
+    'sectionList'
+  );
+}
+
+export const SectionList = createSectionList();
